4c2a8cab0e39f7ea5ddcdbf40fd0892cbfd241b3
Jirat Bunme <jiatikan1993@gmail.com>
2020-02-14 11:50:23 +0700

    1.0.2

---
+++ delete_profile.php
@@ -0,0 +1,35 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'ionicapp';
+
+$connect = new mysqli($servername, $username, $password, $dbname);
+$connect->set_charset('utf8');
+
+if ($connect->connect_error) {
+    die("การเชื่อมต่อผิดพลาด : " . $connect->connect_error);
+}
+
+
+    $id = $_GET["id"];
+
+    $sql = "delete from profiles where id = $id";
+
+    // $result = $connect->query($sql);
+
+    if ($connect->query($sql) === true) {
+        $connect->close();
+        return true;
+
+    } else {
+        $connect->close();
+        return $connect->error;
+    
+    $connect->close();
+
+}
